47334d7400e580813a4e979bded979b93d40a912
BT (Grem) Jamiel <brianthomasjamiel@gmail.com>
2023-03-11 21:00:35 -0800

    code chunk

---
+++ .history/random_animes_20230311210010.py
@@ -0,0 +1,16 @@
+from random import choice
+
+# Create list of animes
+
+
+
+
+
+# input mood
+print('what mood are you in?')
+mood = input
+
+# loop through and find a matching mood 
+for item in animes:
+    if item[1] == mood:
+        print(mood + ' anime: ' + item[0])
